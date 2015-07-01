dd33f89b4cc72a011f736ad1dc5ed5c61423d86c
reinhold <reinhold@web.de>
2015-07-01 10:07:13 +0200

    User-Klasse

---
+++ App_Code/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Zusammenfassungsbeschreibung für User
+/// </summary>
+public class User
+{
+	public User()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public String Email { get; set; }
+    public String Kennwort {get;set;}
+}
