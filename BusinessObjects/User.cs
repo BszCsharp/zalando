080872d214cd4c5bf0fb3381169a72a80b3423ea
reinhold <reinhold@web.de>
2015-07-08 09:25:50 +0200

    User-Klasse

---
+++ BusinessObjects/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessObjects
+{
+    /// <summary>
+    /// Zusammenfassungsbeschreibung für User
+    /// </summary>
+    public class User
+    {
+        public User()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+        public Int32 Id { get; set; }
+        public String Email { get; set; }
+        public String Kennwort { get; set; }
+    }
+}
