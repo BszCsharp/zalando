26af7eb2426463d3097cc9a54a188f9025ea40f2
reinhold <reinhold@web.de>
2015-07-08 09:34:02 +0200

    Business

---
+++ BusinessObjects/Angebot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class Angebot
+    {
+        public Int32 Id { get; set; }
+        public String Anghebotstext { get; set; }
+        public override string ToString()
+        {
+            return this.Anghebotstext;
+        }
+    }
+}
---
+++ BusinessObjects/Bestellung.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class Bestellung
+    {
+        Int32 Id { get; set; }
+        String Text { get; set; }
+        DateTime Datum { get; set;  }
+    }
+}
